d30d031dc29b95dc9041d12d5b5559c0f1c9e9a0
Pau Bertolin <paulaberos@gmail.com>
2023-03-30 21:31:22 +0200

    create services

---
+++ src/services/userService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+class UserService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/user`
+    });
+
+    this.api.interceptors.request.use(config => {
+      const storedToken = localStorage.getItem('authToken');
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  getUserData() {
+    return this.api.get('/').then(({ data }) => data).catch(err => console.error(err));
+  }
+
+  editUserData(body) {
+    return this.api.put('/', body).then(({ data }) => data).catch(err => console.error(err));
+  }
+
+  deleteUserData() {
+    return this.api.delete('/').then((response) => response.data).catch(err => console.error(err));
+  }
+}
+
+const userService = new UserService();
+
+export default userService;
