ac1e3d8b9b053ff5b64946908b15dc8bc2006e0e
Gerard Solanes <gerardsolher@gmail.com>
2023-03-31 08:55:17 +0200

    Implements camera setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProviderWrapper } from './context/AuthContext';
 import './index.css';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
