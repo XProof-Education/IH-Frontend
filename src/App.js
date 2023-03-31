0f22199b39a613d2e2e39ca3a4de45140af74c4a
Pau Bertolin <paulaberos@gmail.com>
2023-03-31 18:28:41 +0200

    add react tostify and modified button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+// import { Toaster } from 'react-hot-toast';
 import Home from './views/Home';
 import ErrorPage from './views/ErrorPage';
 import NotFound from './views/NotFound';
@@ -14,13 +14,12 @@ import EditProfile from './views/Profile/EditProfile';
 function App() {
   return (
     <div className="App">
-      <Toaster/>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/profile" element={<IsPrivate><Profile /></IsPrivate>} />
+        <Route path="/edit-profile" element={<IsPrivate><EditProfile /></IsPrivate>} />
         <Route path="/private" element={<IsPrivate><PrivateView /></IsPrivate>} />
         <Route path="/error" element={<ErrorPage />} />
         <Route path="*" element={<NotFound />} />
