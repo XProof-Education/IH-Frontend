04eb75b3a3801f168f21db0c1be28b5724910045
Pau Bertolin <paulaberos@gmail.com>
2023-04-02 21:36:42 +0200

    ListCard logic and add this to Exercises view

--- src/views/Exercises/Exercises.jsx
+++ src/views/Exercises/Exercises.jsx
@@ -1,11 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/Header/Navbar';
+import ListCard from '../../components/Cards/ListCard';
+import exercisesService from '../../services/exercicesService';
+
 
 const Exercises = () => {
+  const [exercises, setExercises] = useState([]);
+
+  const getExercises = async () => {
+    try {
+      const response = await exercisesService.getAllExercises();
+      console.log(response.teacherExercisesData)
+      setExercises(response.teacherExercisesData)
+    } catch (error) {
+      console.error(error)
+    } 
+  }
+
+  useEffect(() => {
+    getExercises();
+  }, [])
   return (
     <div>
       <Navbar color="#FF6230" content="editProfile" backGround="true"/>
       <h1>My exercises</h1>
-      
+      <ListCard props={exercises}/>
     
     </div>
   )
