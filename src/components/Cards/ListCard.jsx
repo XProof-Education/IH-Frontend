f0b9d9487817fea4ca7f99cea46260bc5c298ef6
Pau Bertolin <paulaberos@gmail.com>
2023-04-02 21:15:48 +0200

    create cards, implement ProfileCard to profile view

---
+++ src/components/Cards/ListCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const ListCard = (props) => {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default ListCard;
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useAuth } from '../../hooks/useAuth';
 import Navbar from "../../components/Header/Navbar";
+import ProfileCard from "../../components/Cards/ProfileCard";
 
 
 const Profile = () => {
+  const {user} = useAuth()
   return (
     <div>
-      <Navbar color="#FF6230" content="profile" backGround="true"/>
+      <Navbar color="#FF6230" content="profile" backGround="true" />
       <h1>This is Profile Page</h1>
-      <Link to='/operations-history'>My operations history</Link>
-      <Link to='/frequent-errors'>My frequent errors</Link>
-      <Link to='/exercises'>Find exercices</Link>
-    
+      {user.role !== 'teacher'
+        ? <ProfileCard sections={[{ title: "my equation history", url: '/operations-history' }, { title: "my frequent errors", url: "/frequent-errors" }, { title: "find exercises", url: "/exercises" }]} />
+        : <ProfileCard sections={[{ title: "my exercises", url: '/exercises' }, { title: "create new exercise", url: "/frequent-errors" }, { title: "my equation history", url: "/operations-history" }]} />}
     </div>
-  )
+  );
   
 }
 export default Profile;
