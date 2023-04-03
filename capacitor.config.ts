ad9b03534a1455e186e22aebd856b7301c775370
Gerard Solanes <gerardsolher@gmail.com>
2023-04-03 15:39:10 +0200

    First ios build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'XProof',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
