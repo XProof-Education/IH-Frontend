dc2d01d05897d5082d174593ddf891c55823fafc
Pau Bertolin <paulaberos@gmail.com>
2023-04-04 12:14:25 +0200

    create loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import yellow from '../assets/loading-transparent.gif';
+
+const Loading = () => {
+  return (
+    <img style={{width:"150px"}} src={yellow} alt="loading"/>
+  )
+}
+
+export default Loading;
