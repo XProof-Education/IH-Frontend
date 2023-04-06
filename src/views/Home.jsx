d500c34ca517c32723058e3b1fcf064793ff092e
Gerard Solanes <gerardsolher@gmail.com>
2023-04-06 19:53:26 +0200

    Adds camera logo

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from '../components/Header/Navbar';
 import Button from '../components/Button';
 import { Link } from 'react-router-dom';
+import CameraIcon from '../components/CameraIcon';
 
 export default function Home() {
   return (
     <div>
       <Navbar color="blue"/>
       <h1>Home</h1>
-      <Link to={'/camera'}><Button color='blue'>Camera</Button></Link>
+      <Link to={'/camera'}><CameraIcon size='70px' color='pink' /></Link>
     </div>
   )
 }
