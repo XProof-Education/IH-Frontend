5ec0be92c6ab650002e73c1beae4015d0fa1c6a4
Pau Bertolin <paulaberos@gmail.com>
2023-04-06 18:45:14 +0200

    add preview photo and fix assign exercise bug

--- src/views/Exercises/EditExercise.jsx
+++ src/views/Exercises/EditExercise.jsx
@@ -6,7 +6,6 @@ import exerciseAssignationsService from '../../services/exerciseAssignationsServ
 import Navbar from '../../components/Header/Navbar';
 import Button from '../../components/Button';
 
-
 const EditExercise = () => {
   const { exerciseId } = useParams(); 
   const initialStateEditExercise = {
@@ -19,8 +18,13 @@ const EditExercise = () => {
   const [query, setQuery] = useState('');
   const [foundUsers, setFoundUsers] = useState([]);
   const [isAssigning, setIsAssigning] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [imagePreviewSolution, setImagePreviewSolution] = useState(null);
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
- 
+
+console.log(editedExercise)
+
   const getData = async () => {
     try {
       const { exerciseData } = await exercisesService.getOneExercise(exerciseId);
@@ -34,26 +38,47 @@ const EditExercise = () => {
       });
       setAssignations(assignations);
       setEditedExercise(exerciseData);
+      setImagePreview(exerciseData.exerciseFile);
+      setImagePreviewSolution(exerciseData.solutionFile);
     } catch (error) {
       console.error(error);
     }
   }
 
-  const handleChange = (e) => {
-    setEditedExercise(prev => {
-      return {
-        ...prev,
-        [e.target.name] : e.target.value
-      }
-    })
-  }
+  // const handleChange = (e) => {
+  //   setEditedExercise(prev => {
+  //     return {
+  //       ...prev,
+  //       [e.target.name] : e.target.value
+  //     }
+  //   })
+  // }
 
   const handleQueryChange = async (e) => {
     setQuery(e.target.value);
   }
 
+  const handleFileUpload = async (e) => {
+    const uploadData = new FormData();
+    uploadData.append('imageUrl', e.target.files[0]);
+    try {
+      const response = await exercisesService.uploadExerciseFile(uploadData);
+      setEditedExercise(prev => {
+        return {
+          ...prev,
+          [e.target.name]: response.fileUrl
+        }
+      })
+    // eslint-disable-next-line
+     {e.target.name === "exerciseFile" ? setImagePreview(response.fileUrl) : setImagePreviewSolution(response.fileUrl)} ;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   const addStudentAssignation = (studentId, userEmail) => {
     setIsAssigning(true);
+    setError(false);
     setAssignations(prev => [
       ...prev,
       {
@@ -71,12 +96,16 @@ const EditExercise = () => {
   }
 
   const handleSubmit = async (e) => {
-    const studentIds = assignations.map(assignation => assignation.studentId);
     e.preventDefault();
+    const studentIds = assignations.map(assignation => assignation.studentId);
     try {
-      await exercisesService.editExercise(exerciseId, editedExercise);
-      await exerciseAssignationsService.editExerciseAssignations(exerciseId, { studentIds });
-      navigate('/exercises');
+      if(studentIds.length !== 0) {
+        await exercisesService.editExercise(exerciseId, editedExercise);
+        await exerciseAssignationsService.editExerciseAssignations(exerciseId, { studentIds });
+        navigate('/exercises');
+      } else {
+        setError('Please assign this exercise');
+      }
     } catch (error) {
       console.error(error);
     }
@@ -117,9 +146,11 @@ const EditExercise = () => {
       <h1>Edit Exercise</h1>
       <form onSubmit={handleSubmit}>
         <label>Upload exercise file</label>
-        <input type="text" name="exerciseFile" onChange={handleChange} value={editedExercise.exerciseFile} required />
+        <input type="file" name="exerciseFile" onChange={(e) => handleFileUpload(e)} required={imagePreview !== null ? false : true} />
+         {imagePreview && <img src={imagePreview} alt="preview file" /> }
         <label>Upload exercise solution</label>
-        <input type="text" name="solutionFile" onChange={handleChange} value={editedExercise.solutionFile} />
+        <input type="file" name="solutionFile" onChange={(e) => handleFileUpload(e)} />
+        {imagePreviewSolution && <img src={imagePreviewSolution} alt="preview file" /> }
         <label>Who do you want to assign this exercise to?</label>
         <input type="text" name="student" onChange={handleQueryChange} value={query}/>
         <Button color="blue" type="submit">Submit changes</Button>
@@ -132,6 +163,7 @@ const EditExercise = () => {
           </div>
           )
       })}
+      {error && <p>Please assign this exercise</p>}
       {foundUsers.length !== 0 && foundUsers.map(user => {
         return (
           <div key={user._id ? user._id : user.notFound}>
