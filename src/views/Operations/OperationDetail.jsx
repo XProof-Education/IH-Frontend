942c139edb6a463b2f689c80b639197e9684b3e0
Pau Bertolin <paulaberos@gmail.com>
2023-04-11 15:09:48 +0200

    added Percentage component and some style

--- src/views/Operations/OperationDetail.jsx
+++ src/views/Operations/OperationDetail.jsx
@@ -4,6 +4,7 @@ import operationsService from '../../services/operationsService';
 import Navbar from '../../components/Header/Navbar';
 import Latex from 'react-latex';
 import Button from '../../components/Button';
+import Footer from '../../components/Footer';
 
 
 const OperationDetail = (props) => {
@@ -24,6 +25,7 @@ const OperationDetail = (props) => {
     operationId = params.operationId
   }
   const [operation, setOperation] = useState(null);
+  console.log(operation)
 
   const getOneOperation = async () => {
     try {
@@ -55,17 +57,31 @@ const OperationDetail = (props) => {
 
   return (
     <div>
-      <Navbar color="#FF6230" content="editProfile" backGround="true"/>
-      <h1>Operation detail</h1>
-      {operation && 
-       <div className='equation'> 
-          <Latex>{`$$${operation.mathLatexSimplified}$$`}</Latex>
-          {operation.isCorrect ? <p>This operation is correct. Good job!</p> : <p>{operation.feedBacks[0].text}</p>}
+      <Navbar color="#FF6230" content="editProfile" backGround="true" />
+      <div className="container-detail">
+        <div className={operation && operation.isCorrect ? "operation-detail-container" : "operation-detail-container-red"}>
+          <div className="title-div">
+            <h1 className="title-style-yellow">Operation detail</h1>
+          </div>
+          {operation &&
+            <div className='equation'>
+              <Latex>{`$$${operation.mathLatexSimplified}$$`}</Latex>
+              {operation.isCorrect
+                ? <div className='operation-feedback'>
+                  <p>This operation is correct. Good job!</p>
+                </div>
+                : <div className='operation-feedback'>
+                  <p>{operation.feedBacks[0].text}</p>
+                </div>}
+            </div>
+          }
+          <div className="button-container">
+            <Button color="pink" action={handleDelete}>Delete</Button>
+          </div>
+          <Footer color="blue" size="70px" />
         </div>
-      }
-      <Button color="pink" action={handleDelete}>Delete</Button>
+      </div>
     </div>
-  )
-  
+  );
 }
 export default OperationDetail;
