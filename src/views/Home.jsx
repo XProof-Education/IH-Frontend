349864cd8ad931c9a47a8e1305a6b23a89cac927
Pau Bertolin <paulaberos@gmail.com>
2023-04-12 21:01:12 +0200

    added slider videos - Home view - left button not working

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Navbar from '../components/Header/Navbar';
 import Footer from '../components/Footer';
-// import Slider from '../components/Slider';
+import Slider from '../components/Slider';
 //import steps from '../assets/images/steps.mp4';
-import steps3D from '../assets/images/steps3D.mp4';
-// import ins1 from '../assets/images/ins1.jpg';
-// import ins2 from '../assets/images/ins2.jpg';
-// import ins3 from '../assets/images/ins3.jpg';
+// import steps3D from '../assets/images/steps3D.mp4';
+import step0 from '../assets/images/step0.mp4';
+import step2 from '../assets/images/step2.mp4';
+import step1 from '../assets/images/step1.mp4';
 
 export default function Home() {
   return (
     <div className="home">
-      <Navbar color="blue"/>
-      {/* <Slider images={[prova2, ins1, ins2, ins3]} /> */}
+      <Navbar color="blue" />
+      
       <div className="video-container">
-      <video loop autoPlay muted>
-        <source src={steps3D} alt="people" type="video/mp4" />
-        </video>
-        </div>
-      <Footer color="yellow" size="70px"/>
+        <Slider steps={[step0, step1, step2]} />
+        {/* <video loop autoPlay muted playsInline>
+          <source src={steps3D} alt="people" type="video/mp4" />
+        </video> */}
+      </div>
+      <Footer color="yellow" size="70px" />
     </div>
-  )
+  );
 }
