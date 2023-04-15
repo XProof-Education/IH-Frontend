7b07f1f6d8ad89d53a8ba2204dcba5076e69052b
Pau Bertolin <paulaberos@gmail.com>
2023-04-15 10:49:08 +0200

    delete Home comments

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Navbar from '../components/Header/Navbar';
 import Footer from '../components/Footer';
 import Slider from '../components/Slider';
-//import steps from '../assets/images/steps.mp4';
-// import steps3D from '../assets/images/steps3D.mp4';
 import step0 from '../assets/images/step-0-l.mp4';
 import step1 from '../assets/images/step-1-l.mp4';
 import step2 from '../assets/images/step-2-l.mp4';
@@ -16,9 +14,6 @@ export default function Home() {
       <Navbar backGround="true" color="blue" />
       <div className="video-container">
         <Slider steps={[step0, step1, step2, step3, step4]} />
-        {/* <video loop autoPlay muted playsInline>
-          <source src={steps3D} alt="people" type="video/mp4" />
-        </video> */}
       </div>
       <Footer color="yellow" size="70px" />
     </div>
