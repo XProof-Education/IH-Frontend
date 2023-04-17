c13ce028e996d93f477154900b49c2c0b68605f4
Pau Bertolin <paulaberos@gmail.com>
2023-04-17 16:52:08 +0200

    changed text logo

--- src/components/Icons/HalfDot.jsx
+++ src/components/Icons/HalfDot.jsx
@@ -14,7 +14,7 @@ const HalfDotColor = (props) => {
   return (
     <svg width={props.size} height={props.size} viewBox="0 0 55 110" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M54.5 55.5003C54.5 85.5997 54.5 110 54.5 110C24.4005 110 0 85.5998 0 55.5003C0 25.4007 24.4005 1.00026 54.5 1.00026C54.5 -3.49996 54.5 25.4007 54.5 55.5003Z" fill={color} />
-      <text x="66" y="56" fill="black" fontSize="28" textAnchor="middle" writingMode="tb" fontWeight="bold" fontFamily="raisonne">{props.text}</text>
+      <text x="71" y="64" fill="black" fontSize="28" textAnchor="middle" writingMode="tb" fontFamily="raisonne">{props.text}</text>
     </svg>
   );
 }
